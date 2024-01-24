505c2306f6acb97ba2de368a12536db16123a44b
m_kudo <m_kudo@nsc-inc.co.jp>
2024-01-24 10:11:07 +0900

    20240124/1011

--- pwa/service_worker.js
+++ pwa/service_worker.js
@@ -1,7 +1,12 @@
-// キャッシュファイルの指定
+
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+// キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-    '/poster-keisuke.github.io/',
+    '/pwa/',
+    '/pwa/css/style.css',
+    '/pwa/drawer.js'
 ];
 
 // インストール処理
