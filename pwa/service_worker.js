ec4731d7689d522e5f7f4f97de8a34e9cc6537e2
m_kudo <m_kudo@nsc-inc.co.jp>
2024-01-24 10:26:39 +0900

    update

--- pwa/service_worker.js
+++ pwa/service_worker.js
@@ -4,29 +4,29 @@
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-    '/pwa/',
-    '/pwa/css/style.css',
-    '/pwa/drawer.js'
+	'/pwa/',
+	'/pwa/css/style.css',
+	'/pwa/drawer.js'
 ];
 
 // インストール処理
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
 });
